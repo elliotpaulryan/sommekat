b366294ba89ac41c6d358cc42973c691d53b7172
Elliot Ryan <elliot@Elliots-MacBook-Air.local>
2026-02-14 16:43:24 +0000

    Add PWA support for iPhone app

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="SommeKat" />
+        <meta name="theme-color" content="#4a1e24" />
+      </head>
       <body className={`min-h-screen text-stone-800 antialiased ${julius.className} ${titleFont.variable}`}>
         {children}
       </body>
